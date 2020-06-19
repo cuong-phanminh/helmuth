acb4059755a9ed0ddf02b53261228a67eeafb590
Cuong Phan <cuong.phan@corp.seadev.com.vn>
2020-06-19 18:37:26 +0700

    build block slider

--- wp-content/themes/seadev-child/block-templates/seadev-cus-background-slider/seadev-cus-background-slider.php
+++ wp-content/themes/seadev-child/block-templates/seadev-cus-background-slider/seadev-cus-background-slider.php
@@ -10,3 +10,54 @@
   <!-- Use a button to pause/play the video with JavaScript -->
   <button id="myBtn" onclick="myFunction()">Pause</button>
 </div>
+
+
+<div id="<?php echo $block_id; ?>" class="seadev-background-slider <?php echo $block_classes; ?>" style="<?php echo $height_style; ?>">
+
+  <div class="slider-content <?php echo $content_classes; ?>" style="<?php echo $content_style; ?>">
+    <?php the_field( 'seadev_block_background_slider_content' ); ?>
+  </div>
+  <div class="slider-wrapper">
+
+  <?php while ( have_rows( 'seadev_block_background_slider_items' ) ) : the_row(); ?>
+
+    <?php
+      $slider_type = get_sub_field( 'seadev_block_background_slider_type' );
+      $background_image = get_sub_field( 'seadev_block_background_slider_image' );
+      $youtube_video_id = get_sub_field('seadev_block_background_slider_youtube');
+      $vimeo_video_id = get_sub_field('seadev_block_background_slider_vimeo');
+      $video = get_sub_field( 'seadev_block_background_slider_video' );
+    ?>
+    
+    <?php if ( $slider_type == 'video' ) : ?>
+
+      <div class="slide-item background-video video">
+        <video class="slide-video slide-media" loop muted preload="metadata" poster="">
+          <source src="<?php echo $video['url']; ?>" type="video/mp4" />
+        </video>
+      </div>
+
+    <?php elseif ( $slider_type == 'youtube' ) : ?>
+
+      <div class="slide-item background-video youtube">
+        <iframe class="embed-player" src="https://www.youtube.com/embed/<?php echo $youtube_video_id; ?>?enablejsapi=1&controls=0&fs=0&iv_load_policy=3&rel=0&showinfo=0&loop=1&Q&start=1&playlist=<?php echo $youtube_video_id; ?>" frameborder="0" allowfullscreen></iframe>
+      </div>
+
+    <?php elseif ( $slider_type == 'vimeo' ) : ?>
+
+      <div class="slide-item background-video vimeo">
+        <iframe class="embed-player slide-media" src="https://player.vimeo.com/video/<?php echo $vimeo_video_id; ?>?api=1&byline=0&portrait=0&title=0&background=1&mute=1&loop=1&autoplay=0id=<?php echo $vimeo_video_id; ?>" width="980" height="520" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+      </div>
+
+    <?php else : ?>
+
+      <div class="slide-item background-image">
+        <div class="slide-item-inner" style="background-image: url(<?php echo $background_image['url']; ?>)"></div>
+      </div>
+
+    <?php endif; ?>
+
+  <?php endwhile; ?>  
+
+  </div>
+</div>
--- wp-content/themes/seadev-child/inc_child/blocks.php
+++ wp-content/themes/seadev-child/inc_child/blocks.php
@@ -1,5 +1,32 @@
 <?php
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+	// Register seadev background slider block
+
+
+// add_action('seadev_acf_register_block_type', 'seadev_add_block_background_slider');
+// function seadev_add_block_background_slider() {
+// check function exists.
+if( function_exists('acf_register_block_type') ) {
+acf_register_block_type(array(
+	'name'              => 'seadev-background-slider',
+	'title'             => __('Background Slider', 'seadev'),
+	'description'       => __('Background Slider', 'seadev'),
+	'render_template'   => get_template_directory() . '/block-templates/seadev-background-slider/seadev-background-slider.php',
+	'enqueue_assets' => function(){
+		wp_enqueue_style( 'seadev-background-slider', get_stylesheet_directory_uri() . '/block-templates/seadev-background-slider/seadev-background-slider.css' );
+		wp_enqueue_script( 'seadev-background-slider', get_stylesheet_directory_uri() . '/block-templates/seadev-background-slider/seadev-background-slider.js', array('jquery'), null, true );
+	},
+	'category'          => 'seadev-block',
+	'icon'              => '',
+	'mode'              => 'auto',
+	'keywords'          => array( 'seadev', 'section', 'background', 'slider' ),
+	'supports'					=> array( 'align' => false ),
+));
+}
+}
+
 add_action('seadev_acf_register_block_type', 'seadev_add_block_cus_background_slider');
 function seadev_add_block_cus_background_slider() {
 	// check function exists.
@@ -20,9 +47,15 @@ function seadev_add_block_cus_background_slider() {
 			'supports'					=> array( 'align' => false ),
 		));
 	}
-
-
 }
+
+
+
+
+
+
+	
+
 
 
 
